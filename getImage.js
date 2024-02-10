9318fc8aed6ac03b10d9f92c41e7771445fbe852
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-02-10 16:08:51 -0500

    imageupload functionality working

--- getImage.js
+++ getImage.js
@@ -1,59 +1,38 @@
 require('dotenv').config();
 const aws = require('aws-sdk');
-const randomstring = require("randomstring");
+const create = require("./dalle3.js");
+const axios = require('axios');
+const qs = require('qs');
 
-const aws_secret_key = process.env.AWS_SECRET_KEY;
-const aws_access_key = process.env.AWS_ACCESS_KEY;
+
+const img = process.env.IMG;
 
 
 
 async function image_query(input) {
 
-    const response = await fetch(
-    "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+    const url = await create(input);
+
+
+    const response = await axios.post("https://api.imgbb.com/1/upload", qs.stringify({
+        image: url,
+        key:img
+    })
+    ,
     {
-        headers: { Authorization: `Bearer ${process.env.HUGGING_FACE}` },
-        method: "POST",
-        body: JSON.stringify({"inputs": input})
-    }
-    );
+         headers: { 'content-type': 'application/x-www-form-urlencoded' },
+
+    })
 
 
-    const result_buffer = await response.arrayBuffer()
-
-    aws.config.update({
-    secretAccessKey: aws_secret_key,
-    accessKeyId: aws_access_key,
-    signatureVersion: 'v4',
-    region: 'us-east-1'
-
-});
-
-const s3 = new aws.S3({
-    params: {
-        Bucket: 'spindle-gt',
-        signatureVersion: 'v4',
-        region: 'us-east-1'
-    }
-});
-
-//create random key
 
 
-   
-let params = {
-            Bucket: 'spindle-gt',
-            Key: randomstring.generate()+'.png',
-            Body: Buffer.from(result_buffer)
-        };
-        let image_response = await s3.upload(params).promise();
-
-        let image_destination = image_response.Location;
+    return response.data.data.url;
 
 
-   
 
-    return image_destination;
+
+
 
 }
 
