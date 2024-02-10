3c67c8f0766e6a192fd2b8f8cdba4fe732fd7506
wlu314 <125327127+wlu314@users.noreply.github.com>
2024-02-10 02:13:46 -0500

    MongoDB
    MongoURI module for encryption and decryption

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3001
 const openAIRouter = require("./openai.js");
+const mongouri = require("./mongouri.js");
 
+dotenv.config();
 
 // receive hashed mongo URI 
+
+app.use('/mongoDB', mongouri)
 
 app.use('/openai', openAIRouter)
 
